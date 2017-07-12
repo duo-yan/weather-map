4b5f61a32bf470a14b7a7fe1ab25517b943b6b7e
Duo-Yan <duo-yan@neruti.com>
2017-07-12 19:31:47 +0800

    updated comments/styles

--- NMjs.js
+++ NMjs.js
@@ -1,13 +1,14 @@
 var temp;
 var humidity;
 var wind;
-var direction;
 var APPID = "e9e239c6585f081bee7b0d7f6045a53f";
 
 function initAutocomplete() {
   var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 4.444997,
-            lng: 106.554199},
+    center: {
+      lat: 4.444997,
+      lng: 106.554199
+    },
     zoom: 6,
     mapTypeId: 'roadmap'
   });
@@ -16,19 +17,19 @@ function initAutocomplete() {
   var searchBox = new google.maps.places.SearchBox(input);
   map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
-  // Bias the SearchBox results towards current map's viewport.
+  // Moving the SearchBox results towards current map's viewport.
   map.addListener('bounds_changed', function() {
     searchBox.setBounds(map.getBounds());
   });
 
-  //remove markers
+  //Remove markers
   var gmarkers = [];
 
- function removeMarkers() {
-   for (i = 0; i < gmarkers.length; i++) {
-    gmarkers[i].setMap(null);
-   }
- }
+  function removeMarkers() {
+    for (i = 0; i < gmarkers.length; i++) {
+      gmarkers[i].setMap(null);
+    }
+  }
 
 
   // Listen for the event fired when the user selects a prediction and retrieve
@@ -38,7 +39,7 @@ function initAutocomplete() {
     if (places.length == 0) {
       return;
     }
-    // For each place, get the icon, name and location.
+    // Get the icon, name and location for each result
     var bounds = new google.maps.LatLngBounds();
     removeMarkers();
     places.forEach(function(place) {
@@ -64,36 +65,36 @@ function initAutocomplete() {
 
       gmarkers.push(markers);
 
-      $(function(){
+      $(function() {
         var api = 'http://api.openweathermap.org/data/2.5/weather?q={' +
-                    markers["title"] + '}&APPID=' + APPID;
+          markers["title"] + '}&APPID=' + APPID;
         $.getJSON({
           dataType: 'json',
           url: api,
-          success: function(data){
+          success: function(data) {
             temp = K2C(data.main.temp);
             humidity = data.main.humidity;
             wind = data.wind.speed;
             icon = data.weather[0].id;
 
 
-            var contentString = '<div id="content">'+
-                '<div id="siteNotice">'+
-                '</div>'+
-                '<h3 id="firstHeading" class="firstHeading">' + markers["title"] + '</h3>'+
-                '<div id="bodyContent">'+
-                '<div> <img id = "icon" width = "75px" src = "imgs/codes/'+ icon +'.png" /> </div>'+
-                '<div> temperature: '+ temp + '&degC</div>' +
-                '<div> humidity: '+ humidity + '%' + '</div>' +
-                '<div> wind speed: '+ wind + 'm/s ' + '</div>' +
+            var contentString = '<div id="content">' +
+              '<div id="siteNotice">' +
+              '</div>' +
+              '<h3 id="firstHeading" class="firstHeading">' + markers["title"] + '</h3>' +
+              '<div id="bodyContent">' +
+              '<div> <img id = "icon" width = "75px" src = "imgs/codes/' + icon + '.png" /> </div>' +
+              '<div> temperature: ' + temp + '&degC</div>' +
+              '<div> humidity: ' + humidity + '%' + '</div>' +
+              '<div> wind speed: ' + wind + 'm/s ' + '</div>' +
 
-                '</div>';
+              '</div>';
 
             var infowindow = new google.maps.InfoWindow({
               content: contentString
             });
 
-            markers.addListener('click',function(){
+            markers.addListener('click', function() {
               infowindow.open(map, markers);
             });
           }
@@ -102,11 +103,9 @@ function initAutocomplete() {
 
 
       //kelvin to celcius
-      function K2C(k){
-      	return Math.round(k - 273.15);
+      function K2C(k) {
+        return Math.round(k - 273.15);
       }
-
-
 
       if (place.geometry.viewport) {
         // Only geocodes have viewport.
